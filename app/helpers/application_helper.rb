d2be4c7d33149886d10498d40e927d1e45845456
Roby Parr <roby.parr@gmail.com>
2019-12-10 20:53:21 +0900

    Fix sidebar navigation styling
    The hover effect was being applied to the whole set of links
    instead of each single link and the navigation just looked ugly
    in general.
    
    I've fixed the hover issue and also made the navigation look a
    little nicer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,27 @@ module ApplicationHelper
       sanitize(title, tags: []) + ' <i class="fas fa-external-link-alt text-sm"></i>'.html_safe
     end
   end
+
+  def active_link_to(text = nil, url = nil, options = {}, &block)
+    if block_given?
+      url = text
+      link_to url, active_link_to_options(url, options), &block
+    else
+      link_to text, url, active_link_to_options(url, options)
+    end
+  end
+
+  private
+
+  def active_link_to_options(url, options)
+    classes = options[:class] || ''
+    classes += ' active' if current_or_subpage?(url)
+
+    options.merge(class: classes)
+  end
+
+  def current_or_subpage?(url)
+    current_page_path = URI::parse(request.original_url).path
+    current_page_path.start_with?(url)
+  end
 end

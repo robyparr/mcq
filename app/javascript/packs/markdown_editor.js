320a9eb6bb0ef95273c9414e217c037cd4ad0001
Roby Parr <roby.parr@gmail.com>
2020-01-21 23:35:17 +0900

    [WIP] Add a basic WYSIWYG editor

---
+++ app/javascript/packs/markdown_editor.js
@@ -0,0 +1,7 @@
+import Vue from 'vue/dist/vue.esm'
+import MarkdownEditor from '../src/markdown_editor'
+
+new Vue({
+  el: '#markdown-editor',
+  components: { MarkdownEditor }
+})
---
+++ app/javascript/src/markdown_editor/index.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="editor">
+    <input type="hidden" name="media_note[content]" v-model="html" />
+    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+      <div class="menubar">
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.bold() }"
+                @click="commands.bold">
+          <i class="fas fa-bold"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.italic() }"
+                @click="commands.italic">
+          <i class="fas fa-italic"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.strike() }"
+                @click="commands.strike">
+          <i class="fas fa-strikethrough"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.underline() }"
+                @click="commands.underline">
+          <i class="fas fa-underline"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.code() }"
+                @click="commands.code">
+          <i class="fas fa-code"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.paragraph() }"
+                @click="commands.paragraph">
+          <i class="fas fa-paragraph"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+                @click="commands.heading({ level: 1 })">
+          <i class="fas fa-heading"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+                @click="commands.heading({ level: 2 })">
+          <i class="fas fa-heading"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+                @click="commands.heading({ level: 3 })">
+          <i class="fas fa-heading"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.bullet_list() }"
+                @click="commands.bullet_list">
+          <i class="fas fa-list-ul"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.ordered_list() }"
+                @click="commands.ordered_list">
+          <i class="fas fa-list-ol"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.blockquote() }"
+                @click="commands.blockquote">
+          <i class="fas fa-quote-right"></i>
+        </button>
+
+        <button class="menubar__button"
+                :class="{ 'is-active': isActive.code_block() }"
+                @click="commands.code_block">
+          <i class="fas fa-code"></i>
+        </button>
+
+        <button class="menubar__button" @click="commands.horizontal_rule">
+          hr
+        </button>
+
+        <button class="menubar__button" @click="commands.undo">
+          <i class="fas fa-undo"></i>
+        </button>
+
+        <button class="menubar__button" @click="commands.redo">
+          <i class="fas fa-redo"></i>
+        </button>
+      </div>
+    </editor-menu-bar>
+
+    <editor-content class="editor__content" :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from 'tiptap-extensions'
+
+export default {
+  components: {
+    EditorContent,
+    EditorMenuBar,
+  },
+  data() {
+    return {
+      html: '',
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History(),
+        ],
+        onUpdate: ({ getJSON, getHTML }) => {
+          this.html = getHTML()
+        },
+        content: `
+          <h2>
+            Hi there,
+          </h2>
+          <p>
+            this is a very <em>basic</em> example of tiptap.
+          </p>
+          <pre><code>body { display: none; }</code></pre>
+          <ul>
+            <li>
+              A regular list
+            </li>
+            <li>
+              With regular items
+            </li>
+          </ul>
+          <blockquote>
+            It's amazing 👏
+            <br />
+            – mom
+          </blockquote>
+        `,
+      }),
+    }
+  },
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>

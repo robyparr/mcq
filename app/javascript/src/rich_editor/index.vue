4a2846addf14bb4e0be4b2491333d5bf2c70ce12
Roby Parr <roby.parr@gmail.com>
2020-02-04 21:02:52 +0900

    Allow note editing and fix a few editor bugs

--- app/javascript/src/rich_editor/index.vue
+++ app/javascript/src/rich_editor/index.vue
@@ -3,85 +3,100 @@
     <input type="hidden" name="media_note[content]" v-model="html" />
     <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
       <div class="menubar">
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.bold() }"
                 @click="commands.bold">
           <i class="fas fa-bold"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.italic() }"
                 @click="commands.italic">
           <i class="fas fa-italic"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.strike() }"
                 @click="commands.strike">
           <i class="fas fa-strikethrough"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.underline() }"
                 @click="commands.underline">
           <i class="fas fa-underline"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.code() }"
                 @click="commands.code">
           <i class="fas fa-code"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.paragraph() }"
                 @click="commands.paragraph">
           <i class="fas fa-paragraph"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.heading({ level: 1 }) }"
                 @click="commands.heading({ level: 1 })">
           <i class="fas fa-heading"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.heading({ level: 2 }) }"
                 @click="commands.heading({ level: 2 })">
           <i class="fas fa-heading"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.heading({ level: 3 }) }"
                 @click="commands.heading({ level: 3 })">
           <i class="fas fa-heading"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.bullet_list() }"
                 @click="commands.bullet_list">
           <i class="fas fa-list-ul"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.ordered_list() }"
                 @click="commands.ordered_list">
           <i class="fas fa-list-ol"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.blockquote() }"
                 @click="commands.blockquote">
           <i class="fas fa-quote-right"></i>
         </button>
 
-        <button class="menubar__button"
+        <button type="button"
+                class="menubar__button"
                 :class="{ 'is-active': isActive.code_block() }"
                 @click="commands.code_block">
           <i class="fas fa-code"></i>
         </button>
 
-        <button class="menubar__button" @click="commands.horizontal_rule">
+        <button type="button"
+                class="menubar__button"
+                @click="commands.horizontal_rule">
           hr
         </button>
       </div>
@@ -113,15 +128,18 @@ import {
 } from 'tiptap-extensions'
 
 export default {
+  props: [
+    'content',
+  ],
   components: {
     EditorContent,
     EditorMenuBar,
   },
   data() {
     return {
-      html: '',
+      html: this.content,
       editor: new Editor({
-        content: this.html || 'Type your note here...',
+        content: this.content,
         extensions: [
           new Blockquote(),
           new BulletList(),

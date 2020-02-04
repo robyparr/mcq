9949cf598fd1c9f61642817d5b47da48389609d5
Roby Parr <roby.parr@gmail.com>
2020-02-04 19:45:42 +0900

    Rename MarkdownEditor to RichEditor

--- app/javascript/packs/markdown_editor.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue/dist/vue.esm'
-import MarkdownEditor from '../src/markdown_editor'
-
-window.renderMarkdownEditor = function(el) {
-  new Vue({
-    el: el,
-    components: { MarkdownEditor }
-  })
-}

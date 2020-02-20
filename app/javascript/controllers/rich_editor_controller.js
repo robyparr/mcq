1ea7d105e16588b380e8471ec7dcbacd323f5b85
Roby Parr <roby.parr@gmail.com>
2020-02-20 23:14:47 +0900

    Replace vue-based tiptap editor with quill

---
+++ app/javascript/controllers/rich_editor_controller.js
@@ -0,0 +1,69 @@
+import { Controller } from 'stimulus'
+import _ from 'lodash'
+
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css';
+
+export default class extends Controller {
+  static targets = [
+    'hiddenInput',
+    'editorWrapper',
+  ]
+
+  hiddenInputTemplate = _.template(`
+    <input type="hidden"
+           name="<%- name %>"
+           data-target="<%- target %>"
+           value="<%- value %>" />
+  `)
+
+  editorWrapperTemplate = _.template(`
+    <div data-target="rich-editor.editorWrapper"><%= content %></div>
+  `)
+
+  connect() {
+    this.insertHiddenInput()
+    this.renderEditor()
+  }
+
+  insertHiddenInput() {
+    const targetString = _([
+      'rich-editor.hiddenInput',
+      this.data.get('inputTargets'),
+    ]).compact().join(' ')
+
+    const inputTemplate = this.hiddenInputTemplate({
+      name:   this.data.get('inputName'),
+      target: targetString,
+      value:  this.data.get('content'),
+    })
+
+    this.element.insertAdjacentHTML('beforeend', inputTemplate)
+  }
+
+  renderEditor() {
+    const wrapperTemplate = this.editorWrapperTemplate({
+      content: this.data.get('content'),
+    })
+
+    this.element.insertAdjacentHTML('beforeend', wrapperTemplate)
+    this.editor = new Quill(this.editorWrapperTarget, {
+      theme: 'snow',
+      placeholder: 'Your note...',
+      modules: {
+        toolbar: [
+          [{ header: [] }, { 'font': [] }],
+          ['bold', 'italic', 'underline', 'strike', 'code'],
+          [{ 'color': [] }, { 'background': [] }],
+          [{ list: 'ordered' }, { list: 'bullet' }, { 'align': [] }],
+          ['blockquote', 'code-block'],
+          ['link', 'image'],
+        ],
+      },
+    })
+
+    this.editor.on('text-change', () => {
+      this.hiddenInputTarget.value = this.editor.root.innerHTML
+    })
+  }
+}

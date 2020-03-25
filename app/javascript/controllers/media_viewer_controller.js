6d8875230968766e67bb264533f39cfd8e9d0a51
Roby Parr <roby.parr@gmail.com>
2020-03-25 17:47:32 +0900

    Fix media viewer close button not clickable while loading

--- app/javascript/controllers/media_viewer_controller.js
+++ app/javascript/controllers/media_viewer_controller.js
@@ -9,22 +9,24 @@ export default class extends ApplicationController {
 
   static templates = {
     default: `
-      <div class="w-full h-full relative">
+      <div class="w-full h-full">
         <div class="toolbar mb-0 justify-end py-1 px-4">
           <button data-action="click->media-viewer#close" class="button icon-only">
             <i class="fas fa-times"></i>
           </button>
         </div>
 
-        <div data-target="media-viewer.loader" class="fully-centered">
-          <i class="fas fa-spinner fa-pulse fa-3x"></i>
-          <span class="text">Loading page...</span>
-        </div>
+        <div class="relative w-full h-full">
+          <div data-target="media-viewer.loader" class="fully-centered">
+            <i class="fas fa-spinner fa-pulse fa-3x"></i>
+            <span class="text">Loading page...</span>
+          </div>
 
-        <iframe src="<%- url %>"
-                class="hidden h-full w-full"
-                data-target="media-viewer.media"
-                data-action="load->media-viewer#urlLoaded" />
+          <iframe src="<%- url %>"
+                  class="hidden h-full w-full"
+                  data-target="media-viewer.media"
+                  data-action="load->media-viewer#urlLoaded" />
+        </div>
       </div>
     `
   }

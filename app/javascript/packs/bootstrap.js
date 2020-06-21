9af787368cb27a133d1e4a041a3b2540f8b505d3
Roby Parr <roby.parr@gmail.com>
2020-06-21 19:32:59 +0900

    Split $() into $() for multiple and $find for single elements
    I don't want to get into a bigger abstraction to allow it to
    smartly know whether $('.maybe-multiple').remove() should apply
    to a single element or multiple elements. So using $() to
    designate that we always expect an array and $find to get a single
    element should be good enough for now.
    
    The main point was simply an alias for the longer
    document.querySelectorAll anyways.

--- app/javascript/packs/bootstrap.js
+++ app/javascript/packs/bootstrap.js
@@ -3,8 +3,9 @@ import '../src/tailwind/application.css'
 window.$ = function(selector) {
   var elements = document.querySelectorAll(selector)
 
-  if (elements.length === 1)
-    return elements[0]
-
   return Array.from(elements)
+}
+
+window.$find = function(selector) {
+  return document.querySelector(selector)
 }

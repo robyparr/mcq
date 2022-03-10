3c568350e45e4b42ce4d2cfdbc047f4e9410c335
Roby Parr <roby.parr@gmail.com>
2022-03-10 20:53:17 +0900

    Update ffi to fix local error

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_08_07_131926) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "activity_logs", force: :cascade do |t|
